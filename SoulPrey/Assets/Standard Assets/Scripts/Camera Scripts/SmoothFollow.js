7f0281d60c691db7577532d636eadf890b0ab641
AlmightyFuzz <tris_t@hotmail.co.uk>
2013-08-01 17:37:33 +0100

    Modified Camera Script
    Changed camera to a fixed position and angle. It has the ability to
    scroll in and out, but it is thought that that functionality may
    ultimately be unnecessary.

--- SoulPrey/Assets/Standard Assets/Scripts/Camera Scripts/SmoothFollow.js
+++ SoulPrey/Assets/Standard Assets/Scripts/Camera Scripts/SmoothFollow.js
@@ -29,25 +29,31 @@ function LateUpdate () {
 		return;
 	
 	// Calculate the current rotation angles
-	var wantedRotationAngle = target.eulerAngles.y;
+	//var wantedRotationAngle = target.eulerAngles.y;
 	var wantedHeight = target.position.y + height;
+	
+	height += Input.GetAxis("Mouse ScrollWheel") * 6.0;
 		
-	var currentRotationAngle = transform.eulerAngles.y;
+	//var currentRotationAngle = transform.eulerAngles.y;
 	var currentHeight = transform.position.y;
 	
 	// Damp the rotation around the y-axis
-	currentRotationAngle = Mathf.LerpAngle (currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+	//currentRotationAngle = Mathf.LerpAngle (currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
 
 	// Damp the height
-	currentHeight = Mathf.Lerp (currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+	currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
 
 	// Convert the angle into a rotation
-	var currentRotation = Quaternion.Euler (0, currentRotationAngle, 0);
+	//var currentRotation = Quaternion.Euler (0, currentRotationAngle, 0);
+	
+	distance += Input.GetAxis("Mouse ScrollWheel") * 6.0;
 	
 	// Set the position of the camera on the x-z plane to:
 	// distance meters behind the target
 	transform.position = target.position;
-	transform.position -= currentRotation * Vector3.forward * distance;
+	transform.position -=  Vector3.forward * distance; //currentRotation
+
+	//wantedHeight += Input.GetAxis("Mouse ScrollWheel") * 6.0;
 
 	// Set the height of the camera
 	transform.position.y = currentHeight;
