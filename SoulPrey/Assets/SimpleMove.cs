6ca572aff00e0b9904336da3114e04234f6798a7
AlmightyFuzz <tris_t@hotmail.co.uk>
2013-08-02 14:29:10 +0100

    Added spawner script.
    Simple spawner script added that allows you to set What, Where and
    HowMany things should be spawned.

---
+++ SoulPrey/Assets/SimpleMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Translate(Vector3.forward * 10f * Time.deltaTime);
+	}
+}
---
+++ SoulPrey/Assets/SpawningScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawningScript : MonoBehaviour {
+	
+	public Transform What;
+	public Vector3 Where;
+	public int HowMany;
+	
+	// Use this for initialization
+	void Start ()
+	{		
+		for (int i = 0; i < HowMany; i++)
+		{
+			//Spawn object
+			Instantiate(What, Where, Quaternion.identity);
+		}
+	}
+	/*
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}*/
+}
